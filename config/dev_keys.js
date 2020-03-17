1198500fb5ce6bcc10ab1adaedffa959c396552b
Oleg Smushkevich <olegs777@bezeqint.net>
2020-03-17 12:33:51 +0200

    delete dev_keys.js

--- config/dev_keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoDB: "mongodb://olegs7777:olegs7777@ds217799.mlab.com:17799/hour_manager",
-  secredOrKey: "bubuna",
-  mailPass: "hourmanager5168335"
-  // sessionSecret: "bubuna"
-};
