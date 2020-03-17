b2cb3d7c0f4444aea0c4514b435abf4706ab7d1f
Oleg Smushkevich <olegs777@bezeqint.net>
2020-03-17 13:14:02 +0200

    ok

--- server.js
+++ server.js
@@ -45,7 +45,15 @@ mongoose
   .catch(err => console.log(err));
 
 //Public Folder
-app.use(express.static(path.join(__dirname, "public")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+  app.use(express.static(path.join(__dirname, "public")));
+} else {
+  app.use(express.static(path.join(__dirname, "public")));
+}
 
 //Views
 app.set("views", path.join(__dirname, "views"));
