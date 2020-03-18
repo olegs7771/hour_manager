1ffaa31c227161c98cac74cd941470eee507b965
olegs7771 <olegs7771@gmail.com>
2020-03-18 20:30:04 +0200

    ok

--- client/src/store/configureStore/configureStoreProd.js
+++ client/src/store/configureStore/configureStoreProd.js
@@ -12,9 +12,9 @@ export default function configureStore(initialState) {
     rootReducer,
     initialState,
     compose(
-      applyMiddleware(...middleware)
-      // window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      //   window.__REDUX_DEVTOOLS_EXTENSION__()
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
     )
   );
   return store;
