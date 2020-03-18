5d80f01a6ed262c263e7847ebcc74892f38b3efc
Oleg Smushkevich <olegs777@bezeqint.net>
2020-03-18 12:35:31 +0200

    OK

--- server.js
+++ server.js
@@ -47,10 +47,12 @@ mongoose
 //Public Folder
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.use(express.static(path.join(__dirname, "public")));
+
   app.get("*", (req, res) => {
     res.setHeader("set-cookie", ["SameSite=Strict;SameSite=Strict"]);
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html", "public")
+    );
   });
   // app.use(express.static(path.join(__dirname, "public")));
 }
