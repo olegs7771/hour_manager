374592823bb70913d73c4392d58b0a8fbaf0dcd5
Oleg Smushkevich <olegs777@bezeqint.net>
2020-04-11 19:33:03 +0300

    ok

--- routers/api/rnapp.js
+++ routers/api/rnapp.js
@@ -36,32 +36,37 @@ router.post("/checkIn_automatic", (req, res) => {
     const dateParam = moment(req.body.timeStart).format("YYYY-MM-D");
 
     //validation passed!
-    // res.json({ message: "Success!" });
+
     //Find jobday for particular date
     const dateFilter = {
       $lt: new Date(dateParam + "T23:59:59"),
       $gt: new Date(dateParam + "T00:00:00"),
     };
     JobDay.find({ date: dateFilter }).then((days) => {
-      if (!days) return res.json({ message: "No days" });
-      const selectedDay = days.find((day) => {
-        console.log("day", day);
-        return day.employee == req.body.id;
-      });
-      res.json(selectedDay);
+      if (days.length === 0) {
+        //No job days today yet .Create new one by checkIn Button
+        new JobDay({
+          employee: req.body.id,
+          timeStart: req.body.timeStart,
+        })
+          .save()
+          .then((jobday) => {
+            res.json(jobday);
+          })
+          .catch((err) => {
+            res.status(400).json({ error: "Error to create" });
+          });
+      } else {
+        console.log("day already created");
+
+        const selectedDay = days.find((day) => {
+          console.log("day", day);
+          return day.employee == req.body.id;
+        });
+        res.json({ selectedDay, message: "sselected day" });
+      }
     });
 
-    // new JobDay({
-    //   employee: req.body.id,
-    //   timeStart: req.body.timeStart,
-    // })
-    //   .save()
-    //   .then((jobday) => {
-    //     res.json(jobday);
-    //   })
-    //   .catch((err) => {
-    //     res.status(400).json({ error: "Error to create" });
-    //   });
     console.log("Authorized");
   });
 });
@@ -99,22 +104,43 @@ router.post("/checkOut_automatic", (req, res) => {
         });
       })
       .catch((err) => {
-        console.log("error to filter", err);
+        console.log("error to filter days", err);
       });
-
-    //   new JobDay({
-    //     employee: req.body.id,
-    //     endStart: req.body.endStart,
-    //   })
-    //     .save()
-    //     .then((jobday) => {
-    //       res.json(jobday);
-    //     })
-    //     .catch((err) => {
-    //       res.status(400).json({ error: "Error to create" });
-    //     });
-    //   console.log("Authorized");
-    // });
   });
 });
+
+//Get Todays CheckIn checkOut Time if day exists
+//For showing in JobdayScreen
+router.post("/get_today_time", (req, res) => {
+  console.log("req.body", req.body);
+  Employee.findOne({ token: req.body.token }).then((emp) => {
+    if (!emp) {
+      return res.status(400).json({ error: "Unauthorized!" });
+    }
+    //Validation Passed
+    //Find all jobdays for today
+
+    const dateFilter = {
+      $lt: new Date(req.body.date + "T23:59:59"),
+      $gt: new Date(req.body.date + "T00:00:00"),
+    };
+    JobDay.find({ date: dateFilter })
+      .then((days) => {
+        if (!days) return res.json({ message: "No jobday" });
+        //Filter found days by Employee Id
+
+        const selectedDay = days.find((day) => {
+          console.log("day", day);
+          return day.employee == req.body.id;
+        });
+        res.json(selectedDay);
+      })
+      .catch((err) => {
+        console.log("error to filter days", err);
+      });
+
+    console.log("Authorized");
+  });
+});
+
 module.exports = router;
