a55f92dfaed39552cd867e9e5439869174738040
Oleg Smushkevich <olegs777@bezeqint.net>
2020-04-29 19:49:04 +0300

    ok

--- routers/api/employee.js
+++ routers/api/employee.js
@@ -172,6 +172,7 @@ router.post(
 //Activation of New Employee from Mail Link with Params
 router.post("/activate", async (req, res) => {
   console.log("req.body", req.body);
+  const randomNum = Math.trunc(Math.random() * 10000000);
 
   //   // // // //Confirm Employee =>  update employee.confirmed=true,code:random number
   const employee = await Employee.findById(req.body.uid);
@@ -179,56 +180,55 @@ router.post("/activate", async (req, res) => {
     res
       .status(400)
       .json({ error: "No such employee. Please contact administrator" });
-  } else {
-    // //Create random code
+  }
+  // //Create random code
 
-    employee.code = Math.trunc(Math.random() * 10000000);
-    employee.confirmed = true;
-    employee.save().then((upEmployee) => {
-      console.log("upEmployee", upEmployee);
-      //Send Email to New Employee
-      const data = {
-        type: "ACTIVATION",
-        name: upEmployee.name,
-        email: upEmployee.email,
-        code: upEmployee.code,
-      };
-      sendMail(data, (cb) => {
-        if (cb.infoMessageid) {
-          console.log(
-            "New Employee received instruction after activation his/her account"
-          );
-          //Send Email to Notify Manager of the Current Project
-        }
+  employee.code = randomNum;
+  employee.confirmed = true;
+  employee.save().then((upEmployee) => {
+    console.log("upEmployee", upEmployee);
+    //Send Email to New Employee
+    const data = {
+      type: "ACTIVATION",
+      name: upEmployee.name,
+      email: upEmployee.email,
+      code: upEmployee.code,
+    };
+    sendMail(data, (cb) => {
+      if (cb.infoMessageid) {
+        console.log(
+          "New Employee received instruction after activation his/her account"
+        );
+        //Send Email to Notify Manager of the Current Project
+      }
+    });
+
+    //     //Update in Project.staff[] employee confirmed:true
+    Project.findById(req.body.projectID).then((project) => {
+      if (!project) {
+        return res.status(400).json({ error: "Can not find project" });
+      }
+      //       //Find eployee in project.staff[]
+      const employeeToUpdate = project.staff.find((item) => {
+        return item.employeeEmail === upEmployee.email;
       });
+      //       console.log("employeeToUpdate", employeeToUpdate);
+      if (employeeToUpdate.confirmed === true) {
+        return res
+          .status(400)
+          .json({ error: "This Account Already been Activated" });
+      }
 
-      //     //Update in Project.staff[] employee confirmed:true
-      Project.findById(req.body.projectID).then((project) => {
-        if (!project) {
-          return res.status(400).json({ error: "Can not find project" });
-        }
-        //       //Find eployee in project.staff[]
-        const employeeToUpdate = project.staff.find((item) => {
-          return item.employeeEmail === upEmployee.email;
-        });
-        //       console.log("employeeToUpdate", employeeToUpdate);
-        if (employeeToUpdate.confirmed === true) {
-          return res
-            .status(400)
-            .json({ error: "This Account Already been Activated" });
-        }
-
-        //Update Activated Employee
-        employeeToUpdate.confirmed = true;
-        project.save().then((upProject) => {
-          //Notify Employee that Accout been Activated!
-          res.json({
-            message: `Dear ${employeeToUpdate.employeeName} your account for HourManager App was successfully activated! You will recieve further instructions to your e-mail. See you soon..`,
-          });
+      //Update Activated Employee
+      employeeToUpdate.confirmed = true;
+      project.save().then((upProject) => {
+        //Notify Employee that Accout been Activated!
+        res.json({
+          message: `Dear ${employeeToUpdate.employeeName} your account for HourManager App was successfully activated! You will recieve further instructions to your e-mail. See you soon..`,
         });
       });
     });
-  }
+  });
 });
 
 //Get Selected Employee
