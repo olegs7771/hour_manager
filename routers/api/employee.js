f549076c73e17a74febc768dd1ece71c3ca09b0d
Oleg Smushkevich <olegs777@bezeqint.net>
2020-04-29 18:09:15 +0300

    ok

--- routers/api/employee.js
+++ routers/api/employee.js
@@ -174,59 +174,57 @@ router.post("/activate", async (req, res) => {
   console.log("req.body", req.body);
 
   //   // // // //Confirm Employee =>  update employee.confirmed=true,code:random number
-  await Employee.findById(req.body.uid).then((employee) => {
-    console.log("employee", employee);
-    if (!employee) {
-      res
-        .status(400)
-        .json({ error: "No such employee. Please contact administrator" });
-    }
-    // //Create random code
-    const ranNum = Math.random() * 10000000;
-    employee.code = Math.trunc(ranNum);
-    employee.confirmed = true;
-    employee.save().then((upEmployee) => {
-      console.log("upEmployee", upEmployee);
-      //Send Email to New Employee
-      const data = {
-        type: "ACTIVATION",
-        name: upEmployee.name,
-        email: upEmployee.email,
-        code: upEmployee.code,
-      };
-      sendMail(data, (cb) => {
-        if (cb.infoMessageid) {
-          console.log(
-            "New Employee received instruction after activation his/her account"
-          );
-          //Send Email to Notify Manager of the Current Project
-        }
-      });
+  const employee = await Employee.findById(req.body.uid);
+  if (!employee) {
+    res
+      .status(400)
+      .json({ error: "No such employee. Please contact administrator" });
+  }
+  // //Create random code
+  const ranNum = Math.random() * 10000000;
+  employee.code = Math.trunc(ranNum);
+  employee.confirmed = true;
+  employee.save().then((upEmployee) => {
+    console.log("upEmployee", upEmployee);
+    //Send Email to New Employee
+    const data = {
+      type: "ACTIVATION",
+      name: upEmployee.name,
+      email: upEmployee.email,
+      code: upEmployee.code,
+    };
+    sendMail(data, (cb) => {
+      if (cb.infoMessageid) {
+        console.log(
+          "New Employee received instruction after activation his/her account"
+        );
+        //Send Email to Notify Manager of the Current Project
+      }
+    });
+  });
 
-      //     //Update in Project.staff[] employee confirmed:true
-      Project.findById(req.body.projectID).then((project) => {
-        if (!project) {
-          return res.status(400).json({ error: "Can not find project" });
-        }
-        //       //Find eployee in project.staff[]
-        const employeeToUpdate = project.staff.find((item) => {
-          return item.employeeEmail === upEmployee.email;
-        });
-        //       console.log("employeeToUpdate", employeeToUpdate);
-        if (employeeToUpdate.confirmed === true) {
-          return res
-            .status(400)
-            .json({ error: "This Account Already been Activated" });
-        }
-        //Update Activated Employee
-        employeeToUpdate.confirmed = true;
-        project.save().then((upProject) => {
-          //Notify Employee that Accout been Activated!
-          res.json({
-            message: `Dear ${employeeToUpdate.employeeName} your account for HourManager App was successfully activated! You will recieve further instructions to your e-mail. See you soon..`,
-          });
-        });
-      });
+  //     //Update in Project.staff[] employee confirmed:true
+  const project = await Project.findById(req.body.projectID);
+
+  if (!project) {
+    return res.status(400).json({ error: "Can not find project" });
+  }
+  //       //Find eployee in project.staff[]
+  const employeeToUpdate = project.staff.find((item) => {
+    return item.employeeEmail === upEmployee.email;
+  });
+  //       console.log("employeeToUpdate", employeeToUpdate);
+  if (employeeToUpdate.confirmed === true) {
+    return res
+      .status(400)
+      .json({ error: "This Account Already been Activated" });
+  }
+  //Update Activated Employee
+  employeeToUpdate.confirmed = true;
+  project.save().then((upProject) => {
+    //Notify Employee that Accout been Activated!
+    res.json({
+      message: `Dear ${employeeToUpdate.employeeName} your account for HourManager App was successfully activated! You will recieve further instructions to your e-mail. See you soon..`,
     });
   });
 });
