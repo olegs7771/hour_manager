ef0fb1a6d453b8b34d068d3889d79c0eca06f09a
Oleg Smushkevich <olegs777@bezeqint.net>
2020-05-24 21:44:32 +0300

    Delete GoogleAPI_Keys_Dev.js

--- client/src/keys/GoogleAPI_Keys_Dev.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const GoogleAPI_Keys_Dev = () => {
-  return {
-    GOOGLE_MAP_API_KEY: "AIzaSyASLLZYTv8JDeXhU4ASMK4U_lyn4gD7vY0",
-    Geocode_ApiKey: "AIzaSyDF9BWn17CT9geI3L-Ff0ujGWwpPHmxvCQ",
-  };
-};
-export default GoogleAPI_Keys_Dev;
